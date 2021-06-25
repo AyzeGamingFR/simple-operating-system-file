b29d727409ef97597836b204a06fa097f9abc4f4
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-06-25 13:09:04 +0200

    Created main.cpp

---
+++ dev-env/main.cpp
@@ -0,0 +1,23 @@
+// default includes
+
+include "main.js"
+
+// default vars
+
+int app = {"founder": "AyzeLYC", "name": "Simple Operating System File Creator | SOSF Creator"};
+
+// default functions
+
+function gui(int xsize, ysize, texts, images, buttons) {
+    
+    
+    
+};
+
+// main namespace
+
+namespace main() {
+    
+    
+    
+};
